387872ec9831ffe473c4edfc59342616a0caba88
JanuBapu <142379561+JanuBapu@users.noreply.github.com>
2025-03-10 08:41:20 +0530

    Update drm.py

--- plugins/drm.py
+++ plugins/drm.py
@@ -1,6 +1,6 @@
 from fileinput import filename
 from pyrogram import filters, Client
-from main import logging
+from main import PRO as bot, prefixes, logging
 from pyrogram.types import Message
 from main import Config
 import os
